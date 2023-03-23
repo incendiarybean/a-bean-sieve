b6519497dd1d4e9d7dd3cab0409a25a9968f2731
incendiarybean <ben.weare12@gmail.com>
2023-03-23 15:09:48 +0000

    Made code block simpler

--- src/main_body.rs
+++ src/main_body.rs
@@ -1,7 +1,7 @@
 use std::{net::SocketAddr, sync::mpsc::Sender, thread};
 
 use eframe::{
-    egui::{self, CentralPanel, Label, Margin, RichText, TextEdit},
+    egui::{self, CentralPanel, Margin, RichText, TextEdit},
     epaint::{Color32, Vec2},
 };
 
@@ -14,7 +14,6 @@ pub fn main_body(
     properties: &mut MainWindow,
     ui: &mut egui::Ui,
     proxy_event_sender: Sender<ProxyEvent>,
-    // request_event_sender: Sender<RequestEvent>,
 ) {
     let panel_frame = egui::Frame {
         fill: ui.ctx().style().visuals.window_fill(),
@@ -31,128 +30,152 @@ pub fn main_body(
     CentralPanel::default()
         .frame(panel_frame)
         .show(ui.ctx(), |ui| {
-            let current_proxy_state = match properties.proxy_status.lock() {
+            let proxy_state = match properties.proxy_status.lock() {
                 Ok(proxy_event) => proxy_event,
                 Err(poisoned) => poisoned.into_inner(),
             };
 
-            match *current_proxy_state {
-                ProxyEvent::Error => {
-                    properties.port_error = "Please check the port is available.".to_string();
-                    properties.start_server_capable = true;
-                }
-                _ => (),
+            let current_proxy_status = match *proxy_state {
+                ProxyEvent::Running => "RUNNING",
+                ProxyEvent::Stopped => "STOPPED",
+                ProxyEvent::Error => "ERROR",
+                ProxyEvent::Terminating => "TERMINATING",
+                ProxyEvent::Terminated => "TERMINATED",
             };
 
-            let label = Label::new("Enter a Port to run on:");
-            ui.add(label);
-            ui.add_space(2.0);
-
-            let input = TextEdit::singleline(&mut properties.port).hint_text("Port, e.g. 8000");
-            let input_response = ui.add(input);
-
-            if input_response.changed() {
-                // TODO: Something about this mess, there is definitely a nicer way
-                if properties.port.char_indices().count() < 2 {
-                    properties.port_error = "Port too short!".to_string();
-                    return;
-                } else {
-                    properties.start_server_capable = true;
-                    properties.port_error = String::default();
-                }
-
-                if properties.port.char_indices().count() > 5 {
-                    properties.port_error = "Port too long!".to_string();
-                    return;
-                } else {
-                    properties.start_server_capable = true;
-                    properties.port_error = String::default();
-                }
-
-                if let Err(_) = properties.port.trim().parse::<u32>() {
-                    properties.port_error = "Port contains invalid characters.".to_string();
-                    properties.start_server_capable = false;
-                    return;
-                } else {
-                    properties.start_server_capable = true;
-                    properties.port_error = String::default();
-                }
+            if current_proxy_status == "ERROR" {
+                properties.port_error = "Please check the port is available.".to_string();
+                properties.start_server_capable = true;
             }
 
-            if !properties.port_error.is_empty() {
-                // properties.start_server_capable = false;
-                ui.add_space(3.0);
-                ui.label(RichText::new(&properties.port_error).color(Color32::LIGHT_RED));
+            if current_proxy_status == "STOPPED" {
+                ui.label(RichText::new("Enter a Port to run on:").size(13.0));
+                ui.add_space(2.0);
+
+                let input = TextEdit::singleline(&mut properties.port).hint_text("Port, e.g. 8000");
+                let input_response = ui.add(
+                    input
+                        .min_size(Vec2 {
+                            x: ui.available_width(),
+                            y: 20.0,
+                        })
+                        .vertical_align(eframe::emath::Align::Center),
+                );
+
+                if input_response.changed() {
+                    // TODO: Something about this mess, there is definitely a nicer way
+                    if properties.port.char_indices().count() < 2 {
+                        properties.port_error = "Port too short!".to_string();
+                        return;
+                    } else {
+                        properties.start_server_capable = true;
+                        properties.port_error = String::default();
+                    }
+
+                    if properties.port.char_indices().count() > 5 {
+                        properties.port_error = "Port too long!".to_string();
+                        return;
+                    } else {
+                        properties.start_server_capable = true;
+                        properties.port_error = String::default();
+                    }
+
+                    if let Err(_) = properties.port.trim().parse::<u32>() {
+                        properties.port_error = "Port contains invalid characters.".to_string();
+                        properties.start_server_capable = false;
+                        return;
+                    } else {
+                        properties.start_server_capable = true;
+                        properties.port_error = String::default();
+                    }
+                }
+
+                if !properties.port_error.is_empty() {
+                    ui.add_space(3.0);
+                    ui.label(
+                        RichText::new(&properties.port_error)
+                            .size(11.0)
+                            .color(Color32::LIGHT_RED),
+                    );
+                }
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::Min), |ui| {
                 ui.with_layout(egui::Layout::left_to_right(egui::Align::BOTTOM), |ui| {
-                    match *current_proxy_state {
-                        ProxyEvent::Running => {
-                            let stop_button = egui::Button::new("Stop Proxy").min_size(Vec2 {
-                                x: ui.available_width() / 2.,
-                                y: 18.,
-                            });
-                            let stop_button_response =
-                                ui.add_enabled(properties.start_server_capable, stop_button);
+                    if current_proxy_status == "RUNNING" {
+                        let stop_button = egui::Button::new("Stop Proxy").min_size(Vec2 {
+                            x: ui.available_width() / 2.,
+                            y: 18.,
+                        });
+                        let stop_button_response =
+                            ui.add_enabled(properties.start_server_capable, stop_button);
 
-                            if stop_button_response.clicked() {
-                                proxy_event_sender.send(ProxyEvent::Terminating).unwrap();
-                            }
+                        if stop_button_response.clicked() {
+                            proxy_event_sender.send(ProxyEvent::Terminating).unwrap();
                         }
-                        _ => {
-                            let start_button = egui::Button::new(match *current_proxy_state {
-                                ProxyEvent::Error => "Retry Proxy",
+                    } else {
+                        let start_button = egui::Button::new(
+                            RichText::new(match current_proxy_status {
+                                "RUNNING" => "Retry Proxy",
                                 _ => "Start Proxy",
                             })
-                            .min_size(Vec2 {
-                                x: ui.available_width() / 2.,
-                                y: 18.,
+                            .size(13.0),
+                        )
+                        .min_size(Vec2 {
+                            x: ui.available_width() / 2.,
+                            y: 18.,
+                        });
+                        let start_button_response =
+                            ui.add_enabled(properties.start_server_capable, start_button);
+
+                        if start_button_response.clicked() {
+                            let port_copy = properties.port.trim().parse::<u16>().unwrap().clone();
+                            let proxy_status = properties.proxy_status.clone();
+
+                            // Create a thread and assign the server to it
+                            // This stops the UI from freezing
+                            thread::spawn(move || {
+                                proxy_service(
+                                    SocketAddr::from(([127, 0, 0, 1], port_copy)),
+                                    proxy_event_sender,
+                                    proxy_status,
+                                    // request_event_sender,
+                                )
                             });
-                            let start_button_response =
-                                ui.add_enabled(properties.start_server_capable, start_button);
-
-                            if start_button_response.clicked() {
-                                let port_copy =
-                                    properties.port.trim().parse::<u16>().unwrap().clone();
-                                let proxy_status = properties.proxy_status.clone();
-
-                                // Create a thread and assign the server to it
-                                // This stops the UI from freezing
-                                thread::spawn(move || {
-                                    proxy_service(
-                                        SocketAddr::from(([127, 0, 0, 1], port_copy)),
-                                        proxy_event_sender,
-                                        proxy_status,
-                                        // request_event_sender,
-                                    )
-                                });
-                            }
                         }
                     }
 
-                    let logs_button = egui::Button::new("View Logs").min_size(Vec2 {
-                        x: ui.available_width(),
-                        y: 18.,
-                    });
+                    let logs_button = egui::Button::new(RichText::new("View Logs").size(13.0))
+                        .min_size(Vec2 {
+                            x: ui.available_width(),
+                            y: 18.,
+                        });
                     ui.add_enabled(false, logs_button);
                 });
 
-                ui.with_layout(egui::Layout::left_to_right(egui::Align::BOTTOM), |ui| {
-                    ui.add(egui::Label::new("Process is currently:"));
-                    ui.add(egui::Label::new(
-                        RichText::new(format!("{:?}", current_proxy_state)).color(
-                            match *current_proxy_state {
-                                ProxyEvent::Running => Color32::LIGHT_GREEN,
-                                _ => Color32::LIGHT_RED,
-                            },
-                        ),
-                    ));
+                ui.with_layout(egui::Layout::top_down(egui::Align::Min), |ui| {
+                    if current_proxy_status == "RUNNING" {
+                        ui.with_layout(egui::Layout::left_to_right(egui::Align::TOP), |ui| {
+                            ui.add(egui::Label::new("Hosting on: "));
+                            ui.add(egui::Label::new(
+                                RichText::new(format!("127.0.0.1:{}", properties.port))
+                                    .color(Color32::LIGHT_GREEN),
+                            ));
+                        });
+                    }
+
+                    ui.with_layout(egui::Layout::left_to_right(egui::Align::BOTTOM), |ui| {
+                        ui.add(egui::Label::new("Process is currently:"));
+                        ui.add(egui::Label::new(
+                            RichText::new(format!("{}", current_proxy_status)).color(
+                                match current_proxy_status {
+                                    "RUNNING" => Color32::LIGHT_GREEN,
+                                    _ => Color32::LIGHT_RED,
+                                },
+                            ),
+                        ));
+                    });
                 });
-
-                // ui.with_layout(egui::Layout::left_to_right(egui::Align::BOTTOM), |ui| {
-
-                // });
             });
         });
 }
